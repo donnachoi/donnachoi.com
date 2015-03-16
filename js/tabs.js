7fbc9c6e7b07b8c76d5cf42d8f25f92f4c3f95ca
Donna <dnm.choi@gmail.com>
2015-03-16 13:04:23 -0400

    styling/modules

---
+++ js/tabs.js
@@ -0,0 +1,9 @@
+$('#techLanguages a').click(function (e) {
+  e.preventDefault();
+  $(this).tab('show');
+})
+
+$('#techSuper a').click(function (e) {
+  e.preventDefault();
+  $(this).tab('show');
+})
